e19109058fad40870ea24fee902a229ac9bf93ea
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-06-28 14:54:05 -0400

    Enqueue scripts and stylesheets

--- btm-events-carousel.php
+++ btm-events-carousel.php
@@ -7,6 +7,19 @@ Author:       Benjamin Reiner
 Author URI:   https://github.com/reinerb/
 */
 
+// Add stylesheets and scripts
+function add_events_carousel_stylesheets () {
+  wp_enqueue_style('splide_main', plugins_url('/styles/splide.min.css', __FILE__));
+  wp_enqueue_style('splide_mods', plugins_url('/styles/splide-mods.css', __FILE__));
+  wp_enqueue_style('post_cards', plugins_url('/styles/post-card.css', __FILE__));
+}
+add_action('wp_enqueue_scripts', 'add_events_carousel_stylesheets');
+
+function add_events_carousel_scripts () {
+  wp_enqueue_script('splide_script', plugins_url('/scripts/splide.min.js'));
+}
+add_action('wp_enqueue_scripts', 'add_events_carousel_scripts');
+
 add_shortcode('eventscarousel', 'create_events_carousel');
 
 // Query posts
